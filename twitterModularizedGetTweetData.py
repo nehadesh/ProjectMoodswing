bbc264e4fbfe797c59c8b6cb752db9db6962c5a6
nehadesh <neha.n.deshpande@gmail.com>
2019-07-29 16:14:37 -0400

    Final version running on lambda with separate csvs per search string

--- twitterModularizedGetTweetData.py
+++ twitterModularizedGetTweetData.py
@@ -4,24 +4,32 @@ import numpy
 import pandas as pd
 import boto3
 import datetime
+import re
+
+main_df = pd.DataFrame('id_str', 'text', 'retweet_count', 'favorite_count', 'created_at', 'coordinates',
+                       'in_reply_to_screen_name', 'user_id_str', 'user_location', 'user_screen_name', 'hashtags',
+                       'mentions', 'group')
 
 
 def lambda_handler(event, context):
     """ This function downloads tweets that are pulled up based on a particular search string"""
 
-    # gets 10 search strings from the SQS search string queue
-    search_strings = download_from_queue(event, context)
+    # gets (group, search query list) tuple for 1 search group from the SQS queue
+    search_group = download_from_queue(event, context)
     api = authenticate()
 
     # extract the group/category and query from each search string
-    for search_string in search_strings:
-        group = search_string[0]
-        query = '\"' + search_string[1] + '\"'
+    group = search_group[0]
+    query_list = search_group[1]
+
+    for query in query_list:
+        # query = '\"' + query + '\"'
         print("Group: " + group + " \nQuery:" + query)
 
         # try to search twitter for the current query, and if it fails due to a rate limit error, wait 15 mins
         try:
             print("Starting search upload...")
+            # search_tweets("'clover network' OR 'clover app' OR 'gyft'", group, api)
             search_tweets(query, group, api)
             print("Search upload complete...")
         except tweepy.RateLimitError:
@@ -34,27 +42,24 @@ def lambda_handler(event, context):
 
 
 def download_from_queue(event, context):
-    # Get the service resource
-    sqs = boto3.resource('sqs')
-
-    # Get the queue
-    queue = sqs.get_queue_by_name(QueueName='twitter_searches.fifo')
     search_strings_list = []
-    count = 0
+    s3 = boto3.resource('s3')
+    for record in event['Records']:
+        filePath = record['body']
 
-    messages = queue.receive_messages(MessageAttributeNames=['Group', 'Query'], MaxNumberOfMessages=10)
+        obj = s3.Object('tweetdump', filePath)
+        body = obj.get()['Body'].read()
+        body = str(body)
+        group = filePath[filePath.find('/') + 1: filePath.find('.txt')]
+        raw_strings = body.split('{')
+        for string in raw_strings:
+            string = string[:string.find("}")]
 
-    for message in messages:
-        count += 1
-        if message.message_attributes is not None:
-            group = message.message_attributes.get('Group').get('StringValue')
-            query = message.message_attributes.get('Query').get('StringValue')
-            search_strings_list.append((group, query))
+            string = string.rstrip("\\r\\n").replace("\\r\\n", " ")
+            string = string.strip()
+            search_strings_list.append(string)
 
-        # Let the queue know that the message is processed
-        message.delete()
-
-    return search_strings_list
+    return (group, search_strings_list[1:])
 
 
 def authenticate():
@@ -65,6 +70,20 @@ def authenticate():
                           'wBjZU436kNqCpANj2NNIs9CVQfSpAGwSKjMrzZnnINqOE')
     api = tweepy.API(auth)
     return api
+
+
+def clean_text(text):
+    print("CLEANING TEXT---------------------------------------------------")
+    tweet = text.lower()
+    # stopwords = set(stopwords.words('english') + list(punctuation) + ['AT_USER', 'URL'])
+    tweet = re.sub('((www\.[^\s]+)|(https?://[^\s]+))', '', tweet)  # remove URLs
+    tweet = re.sub('@[^\s]+', '', tweet)  # remove usernames
+    tweet = re.sub(r'#([^\s]+)', r'\1', tweet)  # remove the # in #hashtag
+    # tweet = word_tokenize(tweet) # remove repeated characters (helloooooooo into hello)
+    # words_in_tweet = [word for word in tweet if word not in stopwords]
+    # tweet_string = str(" ".join(words_in_tweet).encode('utf-8'))
+    print(tweet)
+    return tweet
 
 
 def search_tweets(query, group, api):
@@ -78,25 +97,35 @@ def search_tweets(query, group, api):
     group_list = []
 
     # calls the search function of the API and gets the 100 most recent tweets
-    status_list = api.search(q=query, count=100)
-    if not status_list:
+    status_list = []
+
+    try:
+        status_list = api.search(q=query, count=100)
+    except:
+        print("Failed to find tweets for: " + str(query))
+        pass
+
+    if len(status_list) == 0:
+        print("NOTHING IN PAST 7 DAYS ----------------------------------------------------------------")
         # status list might be empty because we use the Standard Search API
         # which limits our search history to within the past 7 days
         # So in this case... do nothing
         return 0
 
-    status_count = 0
-
     # Data structuring --> removing nested jsons
     for status in status_list:
-        status_count += 1
+        print("STATUS exists -----------------------------------")
 
         group_list.append(group)
         json_str = json.dumps(status._json)
         json_data = json.loads(json_str)
 
+        if (json_data['lang'] != "en"):
+            continue
+
         # extracting required fields from nested user json
         user = json_data['user']
+
         del user['entities']
         user_list.append(user)
         entities = json_data['entities']
@@ -119,6 +148,13 @@ def search_tweets(query, group, api):
 
         del json_data['user']
         del json_data['entities']
+
+        # remove special characters from tweet text
+        text = json_data['text']
+        print("ENTERING CLEAN TEXT--------------------------------------")
+        tweet_string = clean_text(text)
+        print("---------------------------------------- TEXT ----------------------- \n" + tweet_string)
+        json_data['text'] = tweet_string
 
         # final list of tweet jsons with nested jsons removed
         json_list.append(json_data)
@@ -146,15 +182,17 @@ def search_tweets(query, group, api):
         json_df['mentions'] = mention_series
         json_df['group'] = group_series
 
-        # convert data frame to csv and store in s3 bucket
-        print("----------------------------- SAVING AS CSV-----------------------------")
-        s3 = boto3.resource('s3')
-        bucket = s3.Bucket('tweetdump')
-        filepath = '/tmp/tweets' + str(datetime.datetime.now()) + '.csv'
-        with open(filepath, 'w+') as data:
-            # write the csv string to a file called tweets(insert timestamp here).csv
-            data.write(json_df.to_csv())
-        key = 'csv/' + filepath
-        bucket.upload_file(filepath, key)
+        main_df.append(json_df)
 
-    return status_count
+
+def df_to_csv():
+    # convert data frame to csv and store in s3 bucket
+    print("----------------------------- SAVING AS CSV-----------------------------")
+    s3 = boto3.resource('s3')
+    bucket = s3.Bucket('tweetdump')
+    filepath = '/tmp/tweets' + str(datetime.datetime.now()) + '.csv'
+    with open(filepath, 'w+') as data:
+        # write the csv string to a file called tweets(insert timestamp here).csv
+        data.write(main_df.to_csv())
+    key = 'csv/files' + filepath
+    bucket.upload_file(filepath, key)
