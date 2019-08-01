243d451b456a7c4ff5674b093391bb6534f3dbc5
nehadesh <neha.n.deshpande@gmail.com>
2019-08-01 13:11:24 -0400

    Process Sentiment Analysis with fixed string formatting for db entry

--- processSentimentAnalysis.py
+++ processSentimentAnalysis.py
@@ -1,74 +1,61 @@
-# import tar
-# make sure runtime = Python 3.7
-import csv
 import json
-import os
-# import pandas as pd
-import boto3
-from datetime import datetime
-import numpy
-import pandas as pd
-import re
 import pymysql
 import sys
+import boto3
+import csv
+import numpy as np
 
 db_host = "pmoodswingdb.c2mtswumiqvy.us-east-1.rds.amazonaws.com"
 db_user = "pmuser"
 db_password = "butterfly"
 db_name = "pmoodswing"
 
-# calls the AMazon comprehend api
-api = boto3.client('comprehend')
-
-# input bucket
-s3 = boto3.client('s3')
-
-
-# input_path= "tweetdump/csv/files/tmp/" + " "
-# outputpath = "negativetweets/"
 
 def lambda_handler(event, context):
-    bucket = 'tweetdump'
-    if event:
-        file_obj = event["Records"][0]
-        filename = str(file_obj['s3']['object']['key'])
-        fileObj = s3.get_object(Bucket=bucket, Key=filename)
-        file_content = fileObj["Body"].read().decode('utf-8')
+    # Download the processed tweet from the queue
+    # sqs = boto3.resource('sqs')
 
-        entries = file_content.splitlines()
-        entries = entries[1:]
-        for entry in entries:
-            print("ENTRY: " + entry)
-            # fields = entry.split(",")
-            fields = ['{}'.format(x) for x in list(csv.reader([entry], delimiter=',', quotechar='"'))[0]]
-            text = fields[2]
-            print(text)
+    # Get the queue
+    # queue = sqs.get_queue_by_name(QueueName='processed_tweets')        
+    # messages = queue.receive_messages(MessageAttributeNames=['SentimentData'], MaxNumberOfMessages=1)
+    # message = messages[0]
+    # sentimentData = message.message_attributes.get('SentimentData').get('StringValue')
 
-            client = boto3.client('comprehend')
-            sentiment = client.detect_sentiment(Text=text, LanguageCode='en')['Sentiment']
+    # message.delete()
+    for record in event['Records']:
+        dbString = record['body']
 
-            # TODO: Generate the alert score
-            alert_score = 0
+    # dbString = dbString[1:-1]
+    # arrays = dbString.split("]@@@projectmoodswing@@@[")
+    # dbStringRow = arrays[0].split("', '")
+    # dbNumsRow = arrays[1].split(", ")
+    # dbRow = [dbStringRow[0][1:]]
+    # dbRow.append(dbStringRow[1])
+    # dbRow.append(int(dbNumsRow[0]))
+    # dbRow.append(int(dbNumsRow[1]))
+    # dbRow.extend(dbStringRow[2:])
+    # dbRow[-1] = dbRow[-1][:-1]
+    # for field in dbNumsRow[2:]:
+    #     dbRow.append(float(field))
+    # print(dbRow)
 
-            break
-        # df = pd.DataFrame.from_csv(fileObj)
-        # print(df)
-
-        # print(file_content)
-
-    return {"message - ": "hi"}
-
-
-def insert_csv_into_db(event, context):
-    connection = pymysql.connect(host=db_host,
-                                 user=db_user,
-                                 password=db_password,
-                                 database=db_name,
-                                 connect_timeout=10)
+    # dbEntry = str(dbRow)[1:-1]
+    print(dbString)
+    print("Begin connection")
+    connection = pymysql.connect(host=db_host, user=db_user, password=db_password, database=db_name, connect_timeout=10)
+    print("Connected")
     with connection.cursor() as cur:
-        sql = """insert into tweets (id_str, tweet_text, retweet_count, favorite_count, created_at, coordinates, in_reply_to_screen_name, user_id_str, user_location, user_screen_name, hashtags, mentions, product_group, score)
-values (%s);"""
-        cur.execute(sql, (insert_values))
+        sql = """insert into tweets (id_str, tweet_text, retweet_count, favorite_count, created_at, coordinates, 
+        in_reply_to_screen_name, user_id_str, user_location, user_screen_name, hashtags, mentions, product_group, pos_sent, ntrl_sent, neg_sent, mixed_sent, overall_score)
+                values (%s);""" % (dbString)
+
+        # values (%s, %s, %d, %d, %s, %s, %s, %s, %s, %s, %s, %s, %s, %f, %f, %f, %f, %d)
+        cur.execute(sql)
         connection.commit()
         cur.close()
-    connection.close()
+        connection.close()
+
+    return {
+        'statusCode': 200,
+        'body': json.dumps('Hello from Lambda!')
+    }
